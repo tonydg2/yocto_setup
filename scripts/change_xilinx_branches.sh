d352e74ad5e70d5b749716cdf3588928d22bd238
Tony <tonydgoodwin@gmail.com>
2024-06-22 12:29:48 -0400

    automated commit & push

--- scripts/change_xilinx_branches.sh
+++ scripts/change_xilinx_branches.sh
@@ -1,26 +1,28 @@
 #!/bin/bash
 
-cwd=$(pwd)
+CWD=$(pwd)
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+cd "$SCRIPT_DIR"
 
 BRANCH="rel-v2023.2"
 
 cd ../../sources/meta-openamp
-git checkout $BRANCH
+git checkout $BRANCH;git pull;git checkout .;
 cd ../meta-openembedded
-git checkout $BRANCH
+git checkout $BRANCH;git pull;git checkout .;
 cd ../meta-petalinux
-git checkout $BRANCH
+git checkout $BRANCH;git pull;git checkout .;
 cd ../meta-qt5
-git checkout $BRANCH
+git checkout $BRANCH;git pull;git checkout .;
 cd ../meta-ros
-git checkout $BRANCH
+git checkout $BRANCH;git pull;git checkout .;
 cd ../meta-virtualization
-git checkout $BRANCH
+git checkout $BRANCH;git pull;git checkout .;
 cd ../meta-xilinx
-git checkout $BRANCH
+git checkout $BRANCH;git pull;git checkout .;
 cd ../meta-xilinx-tools
-git checkout $BRANCH
+git checkout $BRANCH;git pull;git checkout .;
 cd ../poky
-git checkout $BRANCH
+git checkout $BRANCH;git pull;git checkout .;
 
-cd $cwd
+cd $CWD
